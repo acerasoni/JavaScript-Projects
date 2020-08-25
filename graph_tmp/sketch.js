e6194666a70c51952648a16737959a9f0215cb56
Andrea Giulio Cerasoni <32521086+acerasoni@users.noreply.github.com>
2020-08-25 15:54:38 +0100

    Restructuring tensor product

--- graph_tmp/sketch.js
+++ graph_tmp/sketch.js
@@ -1,11 +1,20 @@
+// Screen settings
 const SCREEN_WIDTH = 1920;
 const SCREEN_HEIGHT = 1080;
+
+// Graph one settings
+const GRAPH_ONE_VERTICES = 8;
+const GRAPH_ONE_EDGES = 3;
+
+// Graph two settings
+const GRAPH_TWO_VERTICES = 8;
+const GRAPH_TWO_EDGES = 3;
 
 function setup() {
     createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    this.graphOne = new graph(8, 5, 10, SCREEN_WIDTH / 3, 15, 30);
-    this.graphTwo = new graph(4, 3, SCREEN_WIDTH / 3 + 20, (SCREEN_WIDTH / 3 * 2) - 10, SCREEN_WIDTH / 3 + 25, 30);
+    this.graphOne = new graph(GRAPH_ONE_VERTICES, GRAPH_ONE_EDGES, 10, SCREEN_WIDTH / 3, 15, 30);
+    this.graphTwo = new graph(GRAPH_TWO_VERTICES, GRAPH_TWO_EDGES, SCREEN_WIDTH / 3 + 20, (SCREEN_WIDTH / 3 * 2) - 10, SCREEN_WIDTH / 3 + 25, 30);
     this.tensorProduct = new tensorProduct(SCREEN_WIDTH / 3 * 2 + 20, SCREEN_WIDTH - 10, graphOne, graphTwo, SCREEN_WIDTH / 3 * 2 + 25, 30);
 }
 
